04a60927acc88f9336d3da7c912d0dcdddf880fb
pranavgaikwad <gaikwad.pranav@gmail.com>
2015-06-19 17:46:20 +0530

    fifth commit

---
+++ app/src/main/java/com/android/acumen/FragmentContact.java
@@ -0,0 +1,24 @@
+package com.android.acumen;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentContact extends Fragment {
+
+
+    public FragmentContact(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        return rootView;
+    }
+}
+
---
+++ app/src/main/java/com/android/acumen/FragmentHome.java
@@ -0,0 +1,35 @@
+package com.android.acumen;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.viewpagerindicator.UnderlinePageIndicator;
+
+public class FragmentHome extends Fragment {
+
+    public FragmentHome(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        WebView homeDescription = (WebView)rootView.findViewById(R.id.homeDescription);
+        homeDescription.setVerticalScrollBarEnabled(false);
+        WebSettings webSettings = homeDescription.getSettings();
+        webSettings.setDefaultFixedFontSize(8);
+        webSettings.setAllowFileAccess(true);
+        //homeDescription.loadData(getString(R.string.home_information),"text/html","utf-8");
+        homeDescription.loadDataWithBaseURL( "file:///android_asset/", getString(R.string.home_information), "text/html", "utf-8", null);
+
+        return rootView;
+    }
+}
+
